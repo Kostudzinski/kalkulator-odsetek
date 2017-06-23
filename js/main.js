4dc35dc5412673a71779d3b22c54145ae3989c98
Konrad Studzinski <kostudzinski@gmail.com>
2017-06-23 13:14:20 +0200

    zrobione

--- js/main.js
+++ js/main.js
@@ -1,58 +1,28 @@
 'use strict';
 
+
+
 function oblicz() {
 
     var wplata = document.getElementById('wplata').value;
-
-    wplata = parseInt;
-
-    var okres = document.getElementById('okres-kapitalicazji').value;
-
-    okres = parseInt;
-
+    var okres = document.getElementById('okres-kapitalizacji').value;
     var iloscLat = document.getElementById('ilosc-lat').value;
-
-    iloscLat = parseInt;
-
-    var oprocentowanie = document.getElementById('oprocentowanie').value / 100;
-
-    oprocentowanie = parseInt;
-
-    var podatekBelki = document.getElementById('podatek');
-
-    podatekBelki = zaznaczony;
-
+    var oprocentowanie = document.getElementById('oprocentowanie').value;
+    var podatekBelki = document.getElementById('podatek').checked;
     var wynik = document.getElementById('wynik');
     
-    var button = document.getElementById('button');
-
-    function obliczWplate() {
-        
-        var oprocentowanieOpodatkowane = 0.19;
-        
-        var rezultat;
-        
-            if (podatekBelki.checked === true) {
-                rezultat = rezultat - (oprocentowanieOpodatkowane * rezultat);
-            }
-        
-        rezultat = wplata * Math.pow(( 1 + (oprocentowanieOpodatkowane / okres ) ), (okres * iloscLat) );
-        
-        return rezultat;
-
-    };
+    var rezultat = wplata * Math.pow(((1 + (oprocentowanie/100)/okres)), okres * iloscLat);
     
+    if(podatekBelki){
+        rezultat = rezultat - (rezultat * 0.19);
+    }
     
-
-            
-    button.onclick = function () {
-    obliczWplate();
-    console.log('test');
-    wynik.innerHTML = rezultat;
-
-};
-    
-    
+    wynik.innerHTML = rezultat.toFixed(2);
 };
 
-oblicz();
+
+
+
+
+    
+    
